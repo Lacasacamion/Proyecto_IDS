560cbd1279be06767577ebc61e26bd8de0269424
gavej <gavej@Tukson>
2023-11-26 21:31:42 -0500

    Modificaciones finales

--- src/main/java/com/mycompany/ids_proyect/Tabla_final.java
+++ src/main/java/com/mycompany/ids_proyect/Tabla_final.java
@@ -2,20 +2,26 @@
 package com.mycompany.ids_proyect;
 
 import java.util.*;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class Tabla_final extends javax.swing.JFrame {
     
     Vector columnas = new Vector();
-    DefaultTableModel mdl_table;
+    Vector cabecera = new Vector();
+    DefaultTableModel mdl_table, table_interpretacion;
     private Gestionador gestionador;
     private static List<Hito> lista_de_hitos;
-
+    private static int h_evaluar;
     
-    public Tabla_final(List<Hito> hitos) {
+    
+    public Tabla_final(List<Hito> hitos,int h_evaluar) {
         initComponents();
         this.lista_de_hitos=hitos;
-        gestionador= new Gestionador();
+        this.h_evaluar=h_evaluar;
+        gestionador= new Gestionador((ArrayList<Hito>) hitos);
+        
+        
         columnas.addElement("N°");
         columnas.addElement("PV");
         columnas.addElement("BAC");
@@ -56,8 +62,13 @@ public class Tabla_final extends javax.swing.JFrame {
             };
             tableModel.addRow(rowData);
         }
-        
-
+        cabecera.addElement("Indicador");
+        cabecera.addElement("Formula");
+        cabecera.addElement("Cálculo");
+        cabecera.addElement("Interpretación");
+        table_interpretacion= new DefaultTableModel(cabecera,0);
+        Table2.setModel(table_interpretacion);
+        gestionador.agregarProyecciones(Table2, h_evaluar);
     }
 
     /**
@@ -72,6 +83,10 @@ public class Tabla_final extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         Table1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        Table2 = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -88,28 +103,72 @@ public class Tabla_final extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(Table1);
 
+        Table2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(Table2);
+        if (Table2.getColumnModel().getColumnCount() > 0) {
+            Table2.getColumnModel().getColumn(0).setPreferredWidth(75);
+            Table2.getColumnModel().getColumn(1).setPreferredWidth(400);
+            Table2.getColumnModel().getColumn(2).setPreferredWidth(200);
+            Table2.getColumnModel().getColumn(3).setPreferredWidth(1000);
+        }
+
+        jLabel1.setText("Tabla de Gestion de Valor Ganado");
+
+        jLabel2.setText("Proyecciones para el hito evaluado");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(7, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 956, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addGap(15, 15, 15))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(395, 395, 395)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(398, 398, 398)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 905, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(68, 68, 68)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(100, Short.MAX_VALUE))
+                .addContainerGap(20, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -150,14 +209,18 @@ public class Tabla_final extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 
-                new Tabla_final(lista_de_hitos).setVisible(true);
+                new Tabla_final(lista_de_hitos, h_evaluar).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable Table1;
+    private javax.swing.JTable Table2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
